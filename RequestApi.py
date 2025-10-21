879979beb366dbd55dfc3b8758c10079e420995c
Renaolol <renanschmeier91@gmail.com>
2025-10-21 11:44:58 -0300

    Update RequestApi.py

--- RequestApi.py
+++ RequestApi.py
@@ -3,8 +3,10 @@ from pprint import pprint
 import streamlit as st
 nome = input("Insira o nome a ser consultado, selecione mais com o separador |")
 url = f'https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}'
-
-response=requests.get(url)
+params = {
+    'sexo':'M'
+}
+response=requests.get(url,params=params)
 try:
     response.raise_for_status()
 except requests.HTTPError as e:
