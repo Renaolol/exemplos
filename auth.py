40781d37a8e723949923c88f7aa6d8ccb9def0c3
Renaolol <renanschmeier91@gmail.com>
2025-10-22 13:25:04 -0300

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,8 @@
+import base64
+login ='meu-login'
+senha = 'minha-senha-super-secreta'
+
+auth_string = f'{login}:{senha}'.encode()
+auth_string = base64.b64encode(auth_string)
+auth_string = auth_string.decode()
+print (auth_string)
