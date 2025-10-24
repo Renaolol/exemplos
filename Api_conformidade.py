ab1cf90e7310fbfb4e5d583e359f29d9fe1ff936
Renaolol <renanschmeier91@gmail.com>
2025-10-24 11:43:44 -0300

    Update Api_conformidade.py
    Adicionado alguns comentários no meio do código para facilitar o entendimento

--- Api_conformidade.py
+++ Api_conformidade.py
@@ -4,12 +4,15 @@ import base64
 from requests_pkcs12 import Pkcs12Adapter
 from pprint import pprint
 from dotenv import load_dotenv
+#Carrega o .env
 load_dotenv()
 session = requests.Session()
 caminho_cert = r"C:\Users\gcont\OneDrive\Desktop\GCONT.pfx" 
 senha = os.getenv("SENHA_CERTIFICADO")
 session.mount("https://",Pkcs12Adapter(pkcs12_filename=caminho_cert,pkcs12_password=senha))
 url = "https://cff.svrs.rs.gov.br/api/v1/consultas/classTrib"
+#Parametros aceitos cst ou NomeCst
 params = {'cst': "000"}
 response = session.get(url,params = params)
+#Utilizado pprint para uma visualização encadeada
 pprint(response.json()[0])
